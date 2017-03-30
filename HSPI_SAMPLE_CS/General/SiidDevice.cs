b8735e8c7490831ce9605fcce723559f0c61b167
newneonova <botline@gmail.com>
2017-03-30 13:36:05 -0600

    progress...

--- HSPI_SAMPLE_CS/General/SiidDevice.cs
+++ HSPI_SAMPLE_CS/General/SiidDevice.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Web;
 
 namespace HSPI_SIID.General
 {
@@ -12,14 +13,17 @@ namespace HSPI_SIID.General
         public Scheduler.Classes.DeviceClass Device { get; set; }
         public PlugExtraData.clsPlugExtraData Extra { get; set; }
         public int Ref { get; set; }
-        public SiidDevice(InstanceHolder Instance,int R)
+        public InstanceHolder Instance { get; set; }
+        public SiidDevice(InstanceHolder I,int R)
         {
+            this.Instance = I;
             this.Ref = R;
             this.Device = (Scheduler.Classes.DeviceClass)Instance.host.GetDeviceByRef(R);
             this.Extra = Device.get_PlugExtraData_Get(Instance.host);
         }
-        public SiidDevice(InstanceHolder Instance, Scheduler.Classes.DeviceClass Dev)
+        public SiidDevice(InstanceHolder I, Scheduler.Classes.DeviceClass Dev)
         {
+            this.Instance = I;
             this.Ref = Dev.get_Ref(Instance.host);
             this.Device = Dev;
             this.Extra = Dev.get_PlugExtraData_Get(Instance.host);
@@ -35,6 +39,17 @@ namespace HSPI_SIID.General
                 }
             }
             return null;
+        }
+
+        public void UpdateExtraData(string key, string value)
+        {
+           
+            var parts = HttpUtility.ParseQueryString(Extra.GetNamed("SSIDKey").ToString());
+            parts[key] = value;
+            Extra.RemoveNamed("SSIDKey");
+            Extra.AddNamed("SSIDKey", parts.ToString());
+            Device.set_PlugExtraData_Set(Instance.host, Extra);
+
         }
 
 
